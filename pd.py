2af53cabbded6ab146be767df2bf1276b55f9bd5
isto.definitivamente.nao.e.um.email <isto.definitivamente.nao.e.um.email@gmail.com>
2009-04-07 14:57:08 +0000

    updated pd.py

--- pd.py
+++ pd.py
@@ -17,14 +17,50 @@ from pd_object_classes.pd_object_collection   import *
 
 
 class Pd:
+    #constructor
     def __init__(self):
-        print "criei comunicacao"
-        c = Communication() 
-        print "criei conexoes"
-        cc  = ConnectionCollection()
-        print "criei pd objects"
-        poc = PdObjectCollection()
+        socket = Communication() 
+        cc     = ConnectionCollection()
+        poc    = PdObjectCollection()
+        
+    #initializing the pd api - must be called before working with it
+    def init(self):
+        self.socket.connectPd()
+        
+    #finishing a pd api session - must be called after working with it
+    def finish(self):
+        self.socket.disconnectPD()
+    
+    #connecting two objects
+    def connect(self, obj):
+        self.cc.create(obj)
+    
+    #removing a given object
+    def disconnect(self, id_src, id_dest, inlet, outlet):
+        self.cc.remove(id_src, id_dest, inlet, outlet)
+    
+    #creating a given object
+    def create(self, obj):
+        self.poc.create(obj)
+    
+    #removing a given object by its id
+    def remove(self, id):
+        self.poc.remove(id)
+    
+    #moving a given object
+    def move(self, id, x, y):
+        self.poc.move(id, x, y)
 
+        
+    ########################################
+    ########################################
+    # TODO - edit objects
+    ########################################
+    ########################################
+    
+    def loadFile(self):
+    
+        
 if __name__ == "__main__": 
     pd = Pd() 
     
