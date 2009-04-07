b8c3dd2bf00d0933e099395d76a5888547ff2b4a
isto.definitivamente.nao.e.um.email <isto.definitivamente.nao.e.um.email@gmail.com>
2009-04-07 01:12:33 +0000

    adding communication classes as well an example

---
+++ communication_classes/communication.py
@@ -0,0 +1,88 @@
+
+##########################################################
+##########################################################
+# description: class that handles pd communication
+#
+# important: based on Frank Barknecht script at:
+# http://markmail.org/message/ohuwrz77hwo3bcwp#query:python%20pdsend+page:1+mid:ybdc6esbu7q53otu+state:results 
+#
+# autor: jeraman
+# date: 06/04/2009
+##########################################################
+##########################################################
+
+#import sys
+from threading  import *
+from socket     import *
+from time       import *  
+from subprocess import *
+
+
+
+
+
+
+
+
+#variable that stores the port number
+PORT = 3000 
+#variable that stores the host
+HOST = "localhost"
+#replace this to where pdsend file is (COMPLETY DIRECTORY)
+PD_DIR = "//Applications/audio/Pd-extended.app/Contents/Resources/bin"
+#replace this to where server.pd is (COMPLETY DIRECTORY)
+SERVER_DIR = "//Users/Jera/Documents/projetos/_pyD/pypd/communication_classes"
+
+
+
+
+
+
+# a thread class that we're gonna use for calling the server.pd patch
+class Pd ( Thread ):
+   def run ( self ):
+       temp = "cd %s && ./pd -nogui %s/server.pd" %(PD_DIR, SERVER_DIR)
+       p = Popen(temp, shell=True)
+
+
+
+
+
+#communication class
+class Communication(socket): 
+    #constructor
+    def __init__(self, host=HOST, port=PORT): 
+        self._port = int(port) 
+        self._host = host 
+        socket.__init__(self)
+        #initializing the server.pd...
+        print "initializing server.pd..."
+        Pd().start()
+        sleep(5)
+        
+    #connecting to pd
+    def connectPd(self): 
+        try: 
+            self.connect((self._host, self._port)) 
+            print "connecting with pd"
+            return True
+        except error, err: 
+            print "Error connecting to %s:%d: %s" % (self._host, self._port, err) 
+            return False
+    
+    #sending a command to pd
+    def sendPd(self, command):
+        try:
+            command += ";" 
+            self.send(command)
+            return True
+        except error, err: 
+            print "Error sending message %s : %s" % (command, err) 
+            return False
+
+    #closing connection
+    def disconnectPD(self): 
+        self.close() 
+        print "closing connection with pd" 
+    
+    
